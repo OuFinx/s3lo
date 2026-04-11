17bf5f5096fe2d196d47d9d7d4ef8a6f4a29d3ed
Volodymyr Stetsenko <uastetsenko@gmail.com>
2026-04-11 10:41:27 +0300

    move recommend into config subcommand, remove standalone recommend command

--- cmd/s3lo/config.go
+++ cmd/s3lo/config.go
@@ -316,9 +316,58 @@ func printImageConfigFields(img image.ImageConfig, indent string) {
 	}
 }
 
+var configRecommendCmd = &cobra.Command{
+	Use:     "recommend <s3-bucket-ref>",
+	Short:   "Show S3 bucket configuration recommendations",
+	Example: `  s3lo config recommend s3://my-bucket/`,
+	Args:    cobra.ExactArgs(1),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		result, err := image.Recommend(cmd.Context(), args[0])
+		if err != nil {
+			return err
+		}
+
+		fmt.Printf("Recommendations for s3://%s/:\n\n", result.Bucket)
+		for i, rec := range result.Recommendations {
+			fmt.Printf("%d. %s\n", i+1, rec.Title)
+			for _, line := range splitLines(rec.Description) {
+				fmt.Printf("   %s\n", line)
+			}
+			fmt.Println()
+		}
+
+		if result.VersioningOn {
+			fmt.Println("(Bucket versioning is enabled - non-current version rule included)")
+			fmt.Println()
+		}
+
+		fmt.Println("Terraform:")
+		for _, line := range splitLines(result.TerraformHCL) {
+			fmt.Printf("  %s\n", line)
+		}
+		return nil
+	},
+}
+
+func splitLines(s string) []string {
+	var lines []string
+	start := 0
+	for i := 0; i < len(s); i++ {
+		if s[i] == '\n' {
+			lines = append(lines, s[start:i])
+			start = i + 1
+		}
+	}
+	if start < len(s) {
+		lines = append(lines, s[start:])
+	}
+	return lines
+}
+
 func init() {
 	configCmd.AddCommand(configSetCmd)
 	configCmd.AddCommand(configGetCmd)
 	configCmd.AddCommand(configRemoveCmd)
+	configCmd.AddCommand(configRecommendCmd)
 	rootCmd.AddCommand(configCmd)
 }
